88b88fe15272137f6064773d19b6546d0751564f
Michael Droettboom <mdboom@gmail.com>
2018-10-29 09:14:05 -0400

    Change schema URL to something obviously fake

--- validate.py
+++ validate.py
@@ -21,6 +21,15 @@ for inpath in Path('schemas').glob('*.yaml'):
     if schema.get('$id'):
         schemas[schema['$id']] = schema
 
+
+class NullResolver(jsonschema.RefResolver):
+    def resolve_remote(self, uri):
+        if uri in self.store:
+            return self.store[uri]
+        if uri == '':
+            return self.referrer
+
+
 for inpath in Path('examples').glob('*.yaml'):
     with open(inpath, 'r') as fp:
         instance = yaml.load(fp)
@@ -33,8 +42,9 @@ for inpath in Path('examples').glob('*.yaml'):
         raise ValueError(f'Invalid schema {schema_url} specified in {inpath}')
 
     schema = schemas[schema_url]
+    resolver = NullResolver.from_schema(schema)
     try:
-        jsonschema.validate(instance, schema)
+        jsonschema.validate(instance, schema, resolver=resolver)
     except Exception as e:
         print(f'{inpath} is invalid')
         raise
